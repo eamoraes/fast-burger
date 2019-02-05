4b4d622a22da48f09e9134b3f012d859d167f817
Ederson Moraes <eamoraes@gmail.com>
2019-02-05 17:54:17 -0200

    project created

---
+++ src/main/java/br/com/fastburger/FastBurgerApplication.java
@@ -0,0 +1,14 @@
+package br.com.fastburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastBurgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastBurgerApplication.class, args);
+	}
+
+}
+
